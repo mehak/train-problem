642f736d334955c4a98ed872fbb8d61725182381
Nathanael Merlin <nate@merlin.uno>
2018-12-14 23:11:35 -0800

    Add functions and other stuff for hunchentoot
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- train-problem.lisp
+++ train-problem.lisp
@@ -1,3 +1,8 @@
+(ql:quickload :hunchentoot)
+(ql:quickload :computable-reals)
+(use-package :computable-reals)
+
+;;; Defun functions for use in hunchentoot
 (defun train-pass (a-speed b-speed distance)
   "Calculates when 2 trains will pass each other, given train a's speed,
   train b's speed and the distance between both trains.  Initial function
@@ -5,3 +10,42 @@
   TODO: take train's acceleration and terminal speed"
   (/ distance
      (+ a-speed b-speed)))
+
+(defun parse-input (input)
+  "wrapper around parse-integer with junk-allowed set to t"
+  (if input
+      (parse-integer input :junk-allowed t)
+      nil))
+
+(defun round-if-rational (number)
+  "Round number if it is rational"
+  (if (rationalp number)
+      (round number)
+      number))
+
+(defun rational-to-mixed (number)
+  "If number is rational, convert it to a mixed numeral (fraction/number)
+   as a string"
+  (multiple-value-bind (whole proper) (floor number)
+    (if (> proper 0)
+        (format nil "~a and ~a" whole proper)
+        whole)))
+
+
+;;; Hunchentoot setup
+(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242))
+
+(hunchentoot:define-easy-handler (trains :uri "/") (a b c)
+  (let ((ta (or (parse-input a)
+                25))
+        (tb (or (parse-input b)
+                25))
+        (tc (or (parse-input c)
+                100)))
+    (setf (hunchentoot:content-type*) "text/plain")
+    (format nil
+            "Two trains are racing towards each other.  Train a is going~%~
+            ~a kph.  Train b is going ~a kph.  When each train reached~%~
+            those velocities they were ~a kilometers apart.  They will~%~
+            crash into each other after ~a hours." ta tb tc
+            (rational-to-mixed (train-pass ta tb tc)))))
